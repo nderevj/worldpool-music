df43b3aa01a10bfd4ac27eb79b7525cf342fe047
nderevj <nick@networkbasedsolutions.com>
2025-12-04 02:13:59 -0800

    trying different drum banks

--- da-funk-by-daft-punk.strudel.js
+++ da-funk-by-daft-punk.strudel.js
@@ -1,12 +1,17 @@
 setcpm(111 / 4);
 
-const drumBank = "akaixr10";
+// const drumBank = "akaixr10"
+// const drumBank = "hr16"; // use rim, or sh instead of cp
+// const drumBank = "rolandr8";
+// const drumBank = "rolandtr707";
+// const drumBank = "sakatadpm48";
+// const drumBank = "yamaharm50";
+const drumBank = "yamahary30"; // best with cp instead of sh
 
 $: s("hh*8, - [- hh*2] - -")
   .bank(drumBank)
   .gain(".33")
-  ._punchcard({ labels: 1 });
 
-$: s("bd sh bd sh, - sd - sd")
+$: s("bd cp bd cp, - sd - sd")
   .bank(drumBank)
-  ._punchcard({ labels: 1 });
+  .gain("1 .5 1 .5")
